a97ce3ff8af65b54ad0147cac374757aec9f2e8c
Sijian Xia <39158528+xiasijian@users.noreply.github.com>
2025-06-13 12:10:31 +0800

    Update ROSE_geneMapper3.12.py
    modify codes

--- ROSE_geneMapper3.12.py
+++ ROSE_geneMapper3.12.py
@@ -1,220 +1,257 @@
-# bamToGFF.py
-# script to grab reads from a bam that align to a .gff file
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+
+"""
+ROSE_geneMapper.py - Maps genes to enhancer regions from ROSE_Main output
+
+Functionality:
+1. Takes enhancer region table from ROSE_Main and maps genes to each enhancer
+2. Creates two output files:
+   - Enhancer-to-gene table (each row represents an enhancer)
+   - Gene-to-enhancer table (each row represents a gene)
+3. By default processes super-enhancers only
+
+Usage:
+python ROSE_geneMapper.py -i <input_file> -g <genome_build> [options]
+"""
+
 import sys
-import re
-import ROSE_utils_new
-from collections import defaultdict
 import os
+from collections import defaultdict
+from typing import List, Dict, Tuple, DefaultDict
 
-#=====================================================================
-#====================MAPPING BAM READS TO GFF REGIONS=================
-#=====================================================================
+# Import ROSE utility functions
+import ROSE_utils
 
-def mapBamToGFF(bamFile, gff, sense='both', extension=200, floor=0, rpm=False, matrix=None):
-    '''maps reads from a bam to a gff'''
-    floor = int(floor)
+
+def mapEnhancerToGene(annotFile: str, 
+                     enhancerFile: str, 
+                     transcribedFile: str = '', 
+                     uniqueGenes: bool = True, 
+                     searchWindow: int = 50000, 
+                     noFormatTable: bool = False) -> Tuple[List[List[str]], List[List[str]]]:
+    """
+    Maps genes to enhancer regions based on genomic coordinates
     
-    # USING BAM CLASS
-    bam = ROSE_utils_new.Bam(bamFile)
-
-    # new GFF to write to
-    newGFF = []
+    Args:
+        annotFile: Path to genome annotation file
+        enhancerFile: Path to enhancer region file from ROSE
+        transcribedFile: Optional file with transcribed genes to consider
+        uniqueGenes: If True, returns only unique gene names
+        searchWindow: Window size (bp) to search for nearby genes
+        noFormatTable: Preserve original table formatting if True
+        
+    Returns:
+        Tuple of two tables:
+        1. Enhancer-to-gene mapping table
+        2. Gene-to-enhancer mapping table
+    """
     
-    # millionMappedReads
-    if rpm:    
-        MMR = round(float(bam.getTotalReads('mapped'))/1000000, 4)
+    # Create dictionary of gene start positions
+    startDict = ROSE_utils.makeStartDict(annotFile)
+    enhancerTable = ROSE_utils.parseTable(enhancerFile, '\t')
+
+    # Get list of transcribed genes to consider
+    if transcribedFile:
+        transcribedTable = ROSE_utils.parseTable(transcribedFile, '\t')
+        transcribedGenes = [line[1] for line in transcribedTable]
     else:
-        MMR = 1
+        transcribedGenes = list(startDict.keys())
 
-    print(f'using a MMR value of {MMR}')
-    
-    if ROSE_utils_new.checkChrStatus(bamFile) == 1:
-        print("has chr")
-        hasChrFlag = 1
+    print('Creating transcript collection')
+    transcribedCollection = ROSE_utils.makeTranscriptCollection(
+        annotFile, 0, 0, 500, transcribedGenes)
+
+    print('Creating TSS collection')
+    tssLoci = []
+    for geneID in transcribedGenes:
+        tssLoci.append(ROSE_utils.makeTSSLocus(geneID, startDict, 0, 0))
+
+    # Create collection of transcription start sites
+    tssCollection = ROSE_utils.LocusCollection(tssLoci, 50)
+
+    # Initialize dictionaries to store gene-enhancer relationships
+    geneDict = {'overlapping': defaultdict(list), 'proximal': defaultdict(list)}
+    rankDict = defaultdict(list)  # Stores enhancer ranks
+    superDict = defaultdict(list)  # Stores super-enhancer status
+    overallGeneList = []  # All genes found in analysis
+
+    # Initialize output tables
+    if noFormatTable:
+        enhancerToGeneTable = [enhancerTable[0] + ['OVERLAP_GENES', 'PROXIMAL_GENES', 'CLOSEST_GENE']]
     else:
-        print("does not have chr")
-        hasChrFlag = 0
-      
-    if isinstance(gff, str):
-        gff = ROSE_utils_new.parseTable(gff, '\t')
-        
-    # setting up a matrix table
-    if matrix:
-        bamName = os.path.basename(bamFile)
-        newGFF.append(['GENE_ID', 'locusLine'] + [f'bin_{n}_{bamName}' for n in range(1, int(matrix)+1)])        
+        enhancerToGeneTable = [enhancerTable[0][0:9] + ['OVERLAP_GENES', 'PROXIMAL_GENES', 'CLOSEST_GENE'] + enhancerTable[5][-2:]]
 
-    # getting and processing reads for gff lines
-    ticker = 0
-    print('Number lines processed')
-    for line in gff:
-        line = line[0:9]
-        if ticker % 100 == 0:
-            print(ticker)
-        ticker += 1
+    geneToEnhancerTable = [['GENE_NAME', 'REFSEQ_ID', 'PROXIMAL_ENHANCERS', 'ENHANCER_RANKS', 'IS_SUPER']]
+
+    # Process each enhancer region
+    for line in enhancerTable:
+        if line[0].startswith(('#', 'R')):
+            continue
+
+        # Create enhancer locus object
+        enhancerString = f"{line[1]}:{line[2]}-{line[3]}"
+        enhancerLocus = ROSE_utils.Locus(line[1], line[2], line[3], '.', line[0])
+
+        # Find overlapping genes (genes whose transcripts overlap enhancer)
+        overlappingLoci = transcribedCollection.getOverlap(enhancerLocus, 'both')
+        overlappingGenes = [locus.ID() for locus in overlappingLoci]
+
+        # Find proximal genes (TSS within search window of enhancer)
+        proximalLoci = tssCollection.getOverlap(
+            ROSE_utils.makeSearchLocus(enhancerLocus, searchWindow, searchWindow), 'both')
+        proximalGenes = [locus.ID() for locus in proximalLoci]
+
+        # Find distal genes (TSS within 1Mb of enhancer)
+        distalLoci = tssCollection.getOverlap(
+            ROSE_utils.makeSearchLocus(enhancerLocus, 1000000, 1000000), 'both')
+        distalGenes = [locus.ID() for locus in distalLoci]
+
+        # Remove duplicates between gene categories
+        overlappingGenes = ROSE_utils.uniquify(overlappingGenes)
+        proximalGenes = ROSE_utils.uniquify(proximalGenes)
+        distalGenes = ROSE_utils.uniquify(distalGenes)
         
-        if not hasChrFlag:
-            line[0] = re.sub(r"chr", r"", line[0])
-            
-        gffLocus = ROSE_utils_new.Locus(line[0], int(line[3]), int(line[4]), line[6], line[1])
-        searchLocus = ROSE_utils_new.makeSearchLocus(gffLocus, int(extension), int(extension))
-        
-        reads = bam.getReadsLocus(searchLocus, 'both', False, 'none')
-        
-        # now extend the reads and make a list of extended reads
-        extendedReads = []
-        for locus in reads:
-            if locus.sense() in ['+', '.']:
-                newLocus = ROSE_utils_new.Locus(locus.chr(), locus.start(), locus.end()+extension, locus.sense(), locus.ID())
-            else:
-                newLocus = ROSE_utils_new.Locus(locus.chr(), locus.start()-extension, locus.end(), locus.sense(), locus.ID())
-            extendedReads.append(newLocus)
-            
-        if gffLocus.sense() in ['+', '.']:
-            senseReads = [x for x in extendedReads if x.sense() in ['+', '.']]
-            antiReads = [x for x in extendedReads if x.sense() == '-']
+        # Remove genes that appear in multiple categories
+        allEnhancerGenes = overlappingGenes + proximalGenes + distalGenes
+        proximalGenes = [g for g in proximalGenes if g not in overlappingGenes]
+        distalGenes = [g for g in distalGenes if g not in overlappingGenes + proximalGenes]
+
+        # Find closest gene to enhancer center
+        closestGene = ''
+        if allEnhancerGenes:
+            enhancerCenter = (int(line[2]) + int(line[3])) // 2
+            distList = [abs(enhancerCenter - startDict[geneID]['start'][0]) 
+                       for geneID in allEnhancerGenes]
+            closestGene = startDict[allEnhancerGenes[distList.index(min(distList))]]['name']
+
+        # Add row to enhancer-to-gene table
+        if noFormatTable:
+            newLine = list(line)
+            newLine.append(','.join(ROSE_utils.uniquify([startDict[x]['name'] for x in overlappingGenes])))
+            newLine.append(','.join(ROSE_utils.uniquify([startDict[x]['name'] for x in proximalGenes])))
+            newLine.append(closestGene)
         else:
-            senseReads = [x for x in extendedReads if x.sense() in ['-', '.']]
-            antiReads = [x for x in extendedReads if x.sense() == '+']
+            newLine = line[0:9]
+            newLine.append(','.join(ROSE_utils.uniquify([startDict[x]['name'] for x in overlappingGenes])))
+            newLine.append(','.join(ROSE_utils.uniquify([start_dict[x]['name'] for x in proximalGenes])))
+            newLine.append(closestGene)
+            newLine += line[-2:]
+        
+        enhancerToGeneTable.append(newLine)
 
-        senseHash = defaultdict(int)
-        antiHash = defaultdict(int)
-
-        # filling in the readHashes             
-        if sense in ['+', 'both', '.']:
-            for read in senseReads:
-                for x in range(read.start(), read.end()+1):
-                    senseHash[x] += 1
-                    
-        if sense in ['-', 'both', '.']:
-            for read in antiReads:
-                for x in range(read.start(), read.end()+1):
-                    antiHash[x] += 1
-
-        # now apply flooring and filtering for coordinates
-        keys = list(set(list(senseHash.keys()) + list(antiHash.keys())))
-        if floor > 0:
-            keys = [x for x in keys if (senseHash[x] + antiHash[x]) > floor]
+        # Record gene-enhancer relationships
+        overallGeneList.extend(overlappingGenes)
+        for refID in overlappingGenes:
+            geneDict['overlapping'][refID].append(enhancerString)
+            rankDict[refID].append(int(line[-2]))
+            superDict[refID].append(int(line[-1]))
             
-        # coordinate filtering
-        keys = [x for x in keys if gffLocus.start() < x < gffLocus.end()]
+        overallGeneList.extend(proximalGenes)
+        for refID in proximalGenes:
+            geneDict['proximal'][refID].append(enhancerString)
+            rankDict[refID].append(int(line[-2]))
+            superDict[refID].append(int(line[-1]))
 
-        # setting up the output table
-        clusterLine = [gffLocus.ID(), str(gffLocus)]
+    # Create gene-to-enhancer table
+    overallGeneList = ROSE_utils.uniquify(overallGeneList)
+    rankOrder = ROSE_utils.order([min(rankDict[x]) for x in overallGeneList])
+    
+    usedNames = []
+    for i in rankOrder:
+        refID = overallGeneList[i]
+        geneName = startDict[refID]['name']
+        
+        if geneName in usedNames and uniqueGenes:
+            continue
+            
+        usedNames.append(geneName)
+        proxEnhancers = geneDict['overlapping'][refID] + geneDict['proximal'][refID]
+        superStatus = max(superDict[refID])
+        enhancerRanks = ','.join(map(str, rankDict[refID]))
+        
+        geneToEnhancerTable.append([
+            geneName, refID, ','.join(proxEnhancers), enhancerRanks, superStatus
+        ])
 
-        if matrix:
-            # getting the binsize
-            binSize = (gffLocus.len()-1)/int(matrix)
-            nBins = int(matrix)
-            if binSize == 0:
-                clusterLine += ['NA'] * int(matrix)
-                newGFF.append(clusterLine)
-                continue
-                
-            n = 0
-            if gffLocus.sense() in ['+', '.', 'both']:
-                i = gffLocus.start()
-                while n < nBins:
-                    n += 1
-                    binKeys = [x for x in keys if i < x < i+binSize]
-                    binDen = float(sum([senseHash[x] + antiHash[x] for x in binKeys]))/binSize
-                    clusterLine.append(round(binDen/MMR, 4))
-                    i += binSize
-            else:
-                i = gffLocus.end()
-                while n < nBins:
-                    n += 1
-                    binKeys = [x for x in keys if i-binSize < x < i]
-                    binDen = float(sum([senseHash[x] + antiHash[x] for x in binKeys]))/binSize
-                    clusterLine.append(round(binDen/MMR, 4))
-                    i -= binSize
-                    
-        newGFF.append(clusterLine)
-        
-    return newGFF
-        
-#=====================================================================
-#============================MAIN METHOD==============================
-#=====================================================================
+    # Sort enhancer table by rank if not preserving format
+    if not noFormatTable:
+        enhancerOrder = ROSE_utils.order([int(line[-2]) for line in enhancerToGeneTable[1:]])
+        sortedTable = [enhancerToGeneTable[0]] + [enhancerToGeneTable[i+1] for i in enhancerOrder]
+        return sortedTable, geneToEnhancerTable
+    
+    return enhancerToGeneTable, geneToEnhancerTable
+
 
 def main():
+    """Main function to parse arguments and run the analysis"""
     from optparse import OptionParser
-    usage = "usage: %prog [options] -b [SORTED BAMFILE] -i [INPUTFILE] -o [OUTPUTFILE]"
-    parser = OptionParser(usage=usage)
     
-    # required flags
-    parser.add_option("-b", "--bam", dest="bam", nargs=1, default=None,
-                    help="Enter .bam file to be processed.")
-    parser.add_option("-i", "--input", dest="input", nargs=1, default=None,
-                    help="Enter .gff or ENRICHED REGION file to be processed.")
+    # Set up command line argument parser
+    parser = OptionParser(usage="usage: %prog [options] -g GENOME -i INPUT_ENHANCER_FILE")
     
-    # output flag
-    parser.add_option("-o", "--output", dest="output", nargs=1, default=None,
-                    help="Enter the output filename.")
+    # Required arguments
+    parser.add_option("-i", "--input", dest="input", 
+                     help="ROSE ranked enhancer or super-enhancer file")
+    parser.add_option("-g", "--genome", dest="genome",
+                     help="Genome build (MM10,MM9,MM8,HG18,HG19,HG38)")
     
-    # additional options
-    parser.add_option("-s", "--sense", dest="sense", nargs=1, default='both',
-                    help="Map to '+','-' or 'both' strands. Default maps to both.")
-    parser.add_option("-f", "--floor", dest="floor", nargs=1, default=0,
-                    help="Sets a read floor threshold necessary to count towards density")    
-    parser.add_option("-e", "--extension", dest="extension", nargs=1, default=200,
-                    help="Extends reads by n bp. Default value is 200bp")
-    parser.add_option("-r", "--rpm", dest="rpm", action='store_true', default=False,
-                    help="Normalizes density to reads per million (rpm)")
-    parser.add_option("-m", "--matrix", dest="matrix", nargs=1, default=None,
-                    help="Outputs a variable bin sized matrix. User must specify number of bins.")
-
-    (options, args) = parser.parse_args()
-
-    print(options)
-    print(args)
-
-    if options.bam:
-        bamFile = options.bam
-        fullPath = os.path.abspath(bamFile)
-        bamName = os.path.splitext(os.path.basename(bamFile))[0]
-        pathFolder = os.path.dirname(fullPath)
-        fileList = os.listdir(pathFolder)
-        hasBai = any(fileName.startswith(bamName) and fileName.endswith('.bai') for fileName in fileList)
-
-        if not hasBai:
-            print('ERROR: no associated .bai file found with bam. Must use a sorted bam with accompanying index file')
-            parser.print_help()
-            sys.exit()
-   
-    if options.sense and options.sense not in ['+', '-', '.', 'both']:
-        print('ERROR: sense flag must be followed by +,-,.,both')
+    # Optional arguments
+    parser.add_option("-l", "--list", dest="geneList",
+                     help="Gene list file to filter through")
+    parser.add_option("-o", "--out", dest="out",
+                     help="Output folder (default: same as input file)")
+    parser.add_option("-w", "--window", dest="window", type="int", default=50000,
+                     help="Search distance for genes (bp, default: 50000)")
+    parser.add_option("-f", "--format", dest="formatTable", action="store_true",
+                     help="Maintain original formatting of input table")
+    
+    # Parse arguments
+    options, _ = parser.parse_args()
+    
+    if not options.input or not options.genome:
         parser.print_help()
-        sys.exit()
+        sys.exit(1)
 
-    if options.matrix:
-        try:
-            int(options.matrix)
-        except ValueError:
-            print('ERROR: User must specify an integer bin number for matrix (try 50)')
-            parser.print_help()
-            sys.exit()
-            
-    if options.input and options.bam:
-        inputFile = options.input
-        gffFile = inputFile
-        bamFile = options.bam
-        
-        if options.output is None:
-            output = os.path.join(os.getcwd(), os.path.basename(inputFile) + '.mapped')
-        else:
-            output = options.output
-            
-        if options.matrix:
-            print('mapping to GFF and making a matrix with fixed bin number')
-            newGFF = mapBamToGFF(bamFile, gffFile, options.sense, int(options.extension), 
-                               options.floor, options.rpm, options.matrix)
-        else:
-            newGFF = mapBamToGFF(bamFile, gffFile, options.sense, int(options.extension), 
-                               options.floor, options.rpm, None)
-        
-        ROSE_utils_new.unParseTable(newGFF, output, '\t')
-    else:
-        parser.print_help()
-                
+    # Set up paths and parameters
+    enhancerFile = options.input
+    window = options.window
+    outFolder = (ROSE_utils.formatFolder(options.out, True) if options.out 
+                else os.path.join(os.path.dirname(enhancerFile), ''))
+    
+    # Get genome annotation file
+    genome = options.genome.upper()
+    print(f'Using {genome} as the genome')
+    
+    cwd = os.getcwd()
+    genomeDict = {
+        'HG18': f'{cwd}/annotation/hg18_refseq.ucsc',
+        'MM9': f'{cwd}/annotation/mm9_refseq.ucsc',
+        'HG19': f'{cwd}/annotation/hg19_refseq.ucsc',
+        'HG38': f'{cwd}/annotation/hg38_refseq.ucsc',
+        'MM8': f'{cwd}/annotation/mm8_refseq.ucsc',
+        'MM10': f'{cwd}/annotation/mm10_refseq.ucsc',
+    }
+    
+    annotFile = genomeDict.get(genome)
+    if not annotFile:
+        print(f"Error: Unsupported genome {genome}")
+        sys.exit(1)
+
+    # Run analysis
+    enhancerToGeneTable, geneToEnhancerTable = mapEnhancerToGene(
+        annotFile, enhancerFile, options.geneList, True, window, options.formatTable)
+    
+    # Write output files
+    baseName = os.path.splitext(os.path.basename(enhancerFile))[0]
+    suffix = f"_{window//1000}KB" if window != 50000 else ""
+    
+    out1 = f"{outFolder}{baseName}_ENHANCER_TO_GENE{suffix}.txt"
+    ROSE_utils.unParseTable(enhancerToGeneTable, out1, '\t')
+    
+    out2 = f"{outFolder}{baseName}_GENE_TO_ENHANCER{suffix}.txt"
+    ROSE_utils.unParseTable(geneToEnhancerTable, out2, '\t')
+
+
 if __name__ == "__main__":
     main()
